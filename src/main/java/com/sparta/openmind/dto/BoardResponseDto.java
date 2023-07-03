83a1aa88bf742e0dde9a4dd3e48299bbd065aac5
Jongwoo Choi <muvnelik@naver.com>
2023-07-03 10:35:17 +0900

    build gradle 추가

--- src/main/java/com/sparta/openmind/dto/BoardResponseDto.java
+++ src/main/java/com/sparta/openmind/dto/BoardResponseDto.java
@@ -1,4 +1,20 @@
 package com.sparta.openmind.dto;
 
+import lombok.Getter;
+
+@Getter
 public class BoardResponseDto {
+    private long id;
+    private String username;
+    private String title;
+    private String contents;
+
+    // ()안을 board 객체로 변경해야함
+
+//    public BoardResponseDto(long id, String username, String title, String contents) {
+//        this.id = id;
+//        this.username = username;
+//        this.title = title;
+//        this.contents = contents;
+//    }
 }
--- src/main/java/com/sparta/openmind/entity/Board.java
+++ src/main/java/com/sparta/openmind/entity/Board.java
@@ -1,5 +1,9 @@
 package com.sparta.openmind.entity;
 // 사용자가 올린 게시글의 조회, 작성, 수정, 삭제
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class Board {
+
+
 }
