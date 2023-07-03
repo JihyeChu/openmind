3a75af65b132de3298536100e61341d544cfd143
Jongwoo Choi <muvnelik@naver.com>
2023-07-03 11:13:18 +0900

    build gradle 추가

--- src/main/java/com/sparta/openmind/contorller/BoardController.java
+++ src/main/java/com/sparta/openmind/contorller/BoardController.java
@@ -1,4 +1,7 @@
 package com.sparta.openmind.contorller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class BoardController {
 }
--- src/main/java/com/sparta/openmind/entity/Board.java
+++ src/main/java/com/sparta/openmind/entity/Board.java
@@ -1,9 +1,37 @@
 package com.sparta.openmind.entity;
 // 사용자가 올린 게시글의 조회, 작성, 수정, 삭제
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.sparta.openmind.dto.BoardRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "Board")
+@NoArgsConstructor
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+
+    public Board(BoardRequestDto requestDto) {
+        this.username =requestDto.getUsername();
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+
+    }
 
 }
