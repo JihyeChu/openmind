afa743483f73db195e071620e097ebce0c1933ab
Hojin37 <hwlsdl37@gmail.com>
2023-07-06 18:48:31 +0900

    Board Delete작동시 Postman에 msg 추가

--- src/main/java/com/sparta/openmind/controller/BoardController.java
+++ src/main/java/com/sparta/openmind/controller/BoardController.java
@@ -3,13 +3,8 @@ package com.sparta.openmind.controller;
 import com.sparta.openmind.dto.BoardRequestDto;
 import com.sparta.openmind.dto.BoardResponseDto;
 import com.sparta.openmind.entity.Board;
-import com.sparta.openmind.entity.Comment;
-import com.sparta.openmind.repository.CommentRepository;
 import com.sparta.openmind.security.UserDetailsImpl;
 import com.sparta.openmind.service.BoardService;
-import com.sparta.openmind.dto.BoardResponseDto;
-import com.sparta.openmind.service.BoardService;
-import com.sparta.openmind.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,10 +23,10 @@ public class BoardController {
     private final BoardService service;
 
     // Get all
-    @GetMapping("/board")
+    /*@GetMapping("/board")
     public List<BoardResponseDto> getContent(){
         return service.getContent();
-    }
+    }*/
 
     // Get
     @GetMapping("/board/{bno}")
@@ -47,23 +42,16 @@ public class BoardController {
         return service.writeContent(requestDto,userDetails.getUser());
     }
 
-    // Delete
+    // Delete - Postman에 msg 추가
     @DeleteMapping("/board/{bno}")
-    public String delete(@PathVariable Integer bno,@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        service.deleteContent(bno,userDetails.getUser());
-        return "삭제가 완료되었습니다.";
-
-    }
-
-    /*@DeleteMapping("/comments/{id}")
-    public ResponseEntity<com.sparta.openmind.dto.ApiResponseDto> deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
+    public ResponseEntity<com.sparta.openmind.dto.ApiResponseDto> deleteContent(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Integer bno) {
         try {
-            CommentService.deleteComment(id, userDetails.getUser());
+            service.deleteContent(bno, userDetails.getUser());
             return ResponseEntity.ok().body(new com.sparta.openmind.dto.ApiResponseDto("댓글 삭제 성공", HttpStatus.OK.value()));
         } catch (RejectedExecutionException e) {
             return ResponseEntity.badRequest().body(new com.sparta.openmind.dto.ApiResponseDto("작성자만 삭제 할 수 있습니다.", HttpStatus.BAD_REQUEST.value()));
         }
-    }*/
+    }
 
     // Put
     @PutMapping("/board/{bno}")
@@ -71,6 +59,10 @@ public class BoardController {
         return service.updateContent(bno,requestDto,userDetails.getUser());
     }
 
+    @GetMapping("/board")
+    public List<BoardResponseDto> getAllBoards(){
+        return service.getAllBoards();
+    }
 
 
 }
